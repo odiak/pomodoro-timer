989382b6d885c75aa60e623dedc71b3264483f2a
Kaido Iwamoto <h6824.kaido@gmail.com>
2015-02-02 01:28:59 +0900

    Show work log

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
   var originalTitle = document.title;
   var INTERVAL = 500;
   var SOUND_FILE = '/sound.mp3';
+  var LS_KEY = 'pomodoroHistory';
   var notification;
   var sound;
+
+  function loadLogsFromLocalStorage() {
+    var _logs = localStorage.getItem(LS_KEY), val;
+    if (_logs) {
+      try {
+        val = JSON.parse(_logs);
+      } catch (e) { }
+    }
+    if (Array.isArray(val)) return val
+    return [];
+  };
+
+  function saveLogsToLocalStorage(logs) {
+    localStorage.setItem(LS_KEY, JSON.stringify(logs));
+  }
 
   function createNotification(title, options) {
     notification = new Notification(title, options);
@@ -34,15 +50,36 @@
 
   window.addEventListener('focus', hideNotification);
 
+  function zeroFill(str, n) {
+    if (typeof str !== 'string') str = '' + str;
+    while (str.length < n) str = '0' + str;
+    return str;
+  }
+
   function formatTime(time) {
     var m, s;
-    m = time / 60 | 0;
-    s = time % 60 | 0;
-    if (m < 10) m = '0' + m;
-    if (s < 10) s = '0' + s;
+    m = zeroFill(time / 60 | 0, 2);
+    s = zeroFill(time % 60 | 0, 2);
     return m + ':' + s;
   }
   Vue.filter('time', formatTime);
+
+  function formatDateTime(date) {
+    var z2 = function(s) { return zeroFill(s, 2); }
+
+    if (!(date instanceof Date)) date = new Date(date);
+    year =    date.getYear() + 1900;
+    month =   z2(date.getMonth() + 1);
+    day =     z2(date.getDate());
+    hours =   z2(date.getHours());
+    minutes = z2(date.getMinutes());
+    seconds = z2(date.getSeconds());
+    return [
+      [year, month, day].join('/'),
+      [hours, minutes, seconds].join(':'),
+    ].join(' ');
+  }
+  Vue.filter('dateTime', formatDateTime);
 
 
   new Vue({
@@ -53,6 +90,8 @@
       startedAt: null,
       time: null,
       intervalId: null,
+      currentType: '',
+      logs: loadLogsFromLocalStorage(),
     },
     methods: {
       setTitle: function() {
@@ -64,8 +103,9 @@
         }
       },
       startTimer: function(minutes) {
-        hideNotification();
         if (this.intervalId) return;
+        hideNotification();
+        this.addLog('Started: ' + this.currentType);
         var time = minutes * 60;
         var that = this;
         this.status = 'started';
@@ -81,32 +121,50 @@
               that.setTitle();
             }
           } else {
-            that.stopTimer(true);
+            that.finishTimer();
           }
         }, INTERVAL);
       },
-      stopTimer: function(notify) {
+      resetTimer: function() {
         if (!this.intervalId) return;
         clearInterval(this.intervalId);
         this.intervalId = null;
         this.status = 'stopped';
         this.remainingTime = 0;
         this.setTitle();
-        if (notify) {
-          createNotification('Pomodoro', {
-            body: 'Timer finished',
-          });
-          playSound();
-        }
+        this.currentType = '';
+      },
+      finishTimer: function() {
+        this.addLog('Finished: ' + this.currentType);
+        createNotification('Pomodoro', {
+          body: 'Timer finished',
+        });
+        playSound();
+        this.resetTimer();
+      },
+      stopTimer: function(notify) {
+        this.addLog('Stopped: ' + this.currentType);
+        this.resetTimer();
       },
       startPomodoro: function() {
+        this.currentType = 'pomodoro';
         this.startTimer(25);
       },
       startBreak: function() {
+        this.currentType = 'break';
         this.startTimer(5);
       },
       startLongBreak: function() {
+        this.currentType = 'long break';
         this.startTimer(15);
+      },
+      addLog: function(message) {
+        this.logs.unshift({timestamp: Date.now(), message: message});
+        saveLogsToLocalStorage(this.logs);
+      },
+      clearLogs: function() {
+        this.logs = [];
+        saveLogsToLocalStorage(this.logs);
       },
     },
   });
